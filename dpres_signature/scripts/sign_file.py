5e6eb419a5f2ae962c08026d3097bf9e17f5b50c
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2017-02-08 17:55:44 +0200

    KDKPAS-773 Add commandline interface and  tests.

---
+++ dpres_signature/scripts/sign_file.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# vim:ft=python
+"""
+Create digital signature file for a file(s) with command::
+
+    sign-file <target1 target2 ...> [rsa-private-key.pem] [signture_path]
+
+To create digital signature you need valid RSA private key. Filename to private
+key is passed as command line parameter.
+
+On succesful signing command returns exit status 0. On failure returns error
+code.
+
+Digital signature a manifest file :file:`varmiste.sig` that lists all files,
+that are to be signed. As a requirement, all files for signing have to be in
+the directory or subdirectory of varmiste.sig given in arguments.
+
+To authenticate creator of manifest it is signed with OpenSSL / SMIME digital
+signature.
+
+For more information about OpenSSL/SMIME signatures and RSA public/private
+keypairs see:
+
+    * http://www.openssl.org/docs/apps/req.html
+    * http://www.madboa.com/geek/openssl/
+
+"""
+
+import sys
+import argparse
+import dpres_signature.signature
+
+
+def parse_arguments(arguments):
+    """Parse commandline arguments."""
+    description = \
+        ("Tool for signing and checking signature of a file.")
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        'targets', nargs='*',
+        help=("File list of files to be signed separated with whitespace."))
+    parser.add_argument(
+        "-c", "--ca-path",
+        default="/etc/ssl/certs",
+        metavar="CAPATH",
+        help=("Path to OpenSSL certificates"))
+    parser.add_argument(
+        "-k", "--key-path",
+        default=None,
+        metavar="KEYPATH",
+        help=("Path to private key"))
+    parser.add_argument(
+        "-s", "--signaturepath",
+        default=None,
+        metavar="SIGNATUREPATH",
+        help=("Signature path"))
+    parser.add_argument(
+        "-w", "--write",
+        default=None,
+        metavar="WRITE",
+        help=("If True, then write signature file."
+              " If false, verify given signature"))
+
+    args = parser.parse_args(arguments[1:])
+    if args.targets == [] and args.write is not None:
+        raise RuntimeError("Missing argument(s): targets")
+    return args
+
+
+def main(arguments):
+    """main."""
+    if arguments is None:
+        arguments = sys.argv
+    args = parse_arguments(arguments)
+    if args.write != "" and args.write is not None:
+        return dpres_signature.signature.signature_write(
+            signature_path=args.signaturepath,
+            key_path=args.key_path,
+            cert_path=args.ca_path,
+            include_patterns=args.targets)
+    else:
+        return dpres_signature.signature.signature_verify(
+            signature_path=args.signaturepath,
+            ca_path=args.ca_path)
+    return dpres_signature.signature.signature_verify(args)
+
+
+if __name__ == '__main__':
+    RETVAL = main()
+    sys.exit(RETVAL)
