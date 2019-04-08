0b86fe61e9c2432842cf1abef23a69b9005ae97b
Thien Tran <thien.tran@csc.fi>
2019-04-08 12:25:20 +0000

    SAPA-61 Add copyright notice

--- dpres_signature/signature.py
+++ dpres_signature/signature.py
@@ -1,4 +1,27 @@
-"""Create and verify SMIME/X509 signed manifest files"""
+"""Create and verify SMIME/X509 signed manifest files
+
+Specific functions have been adapted from a MIT licensed open source solution:
+
+Copyright (c) 2018 Benjamin Peterson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import os
 import six
@@ -70,9 +93,11 @@ def _ensure_str(s, encoding='utf-8', errors='strict'):
       - `str` -> `str`
       - `bytes` -> decoded to `str`
 
-    Direct copy from release 1.12::
+    Adapted from release 1.12 under MIT license::
 
         https://github.com/benjaminp/six/blob/1.12.0/six.py#L872
+
+    Copyright (c) 2018 Benjamin Peterson
     """
     if not isinstance(s, (six.text_type, six.binary_type)):
         raise TypeError("not expecting type '%s'" % type(s))
